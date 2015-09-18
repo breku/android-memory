aaea0908ad3beb52c592920f67e2209e77b78c5f
breku <jakub.brek@gmail.com>
2015-09-18 20:36:19 +0200

    fix to string

--- src/com/thinkfaster/model/shape/MemoryPair.java
+++ src/com/thinkfaster/model/shape/MemoryPair.java
@@ -1,11 +1,11 @@
 package com.thinkfaster.model.shape;
 
-import org.apache.commons.lang3.builder.CompareToBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 /**
  * Created by brekol on 14.09.15.
  */
-public class MemoryPair  {
+public class MemoryPair {
 
     private final MemoryItem item1;
     private final MemoryItem item2;
@@ -22,5 +22,13 @@ public class MemoryPair  {
 
     public MemoryItem getItem2() {
         return item2;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this)
+                .append("item1", item1)
+                .append("item2", item2)
+                .toString();
     }
 }
